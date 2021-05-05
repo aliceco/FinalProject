10e71903758117e189e0076015cf671fe1a78d9d
aliceco <71007397+aliceco@users.noreply.github.com>
2021-05-05 10:56:13 +0200

    finished the film section of film/photos, tried to fix navigation underline, doesnt work yet

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
-
 //animated underline yay
 let items = document.querySelectorAll("nav a");
 
 items.forEach((item) => {
-    item.addEventListener("click", () => {
-        document.querySelector("nav a.active").classList.remove("active");
-        item.classList.add("active");
-    })
+  item.addEventListener("click", () => {
+    document.querySelector("a.active").classList.remove("active");
+    item.classList.add("active");
+  });
+});
 
-})
+/*
+let marker = document.querySelector("#marker");
+let item = document.querySelectorAll("nav a");
+
+function indicator(e) {
+  marker.style.left = e.offsetLeft + "px";
+  marker.style.width = e.offsetWidth + "px";
+}
+
+item.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    indicator(e.target);
+  });
+});
+*/
