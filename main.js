cf56351ddf065cb68441a48b8d7ca9b2a6146b32
aliceco <71007397+aliceco@users.noreply.github.com>
2021-05-11 22:27:53 +0200

    finished the photo gallery

--- main.js
+++ main.js
@@ -8,18 +8,3 @@ items.forEach((item) => {
   });
 });
 
-/*
-let marker = document.querySelector("#marker");
-let item = document.querySelectorAll("nav a");
-
-function indicator(e) {
-  marker.style.left = e.offsetLeft + "px";
-  marker.style.width = e.offsetWidth + "px";
-}
-
-item.forEach((link) => {
-  link.addEventListener("click", (e) => {
-    indicator(e.target);
-  });
-});
-*/
