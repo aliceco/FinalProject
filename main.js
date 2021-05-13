587760461c2aa336d2f914e9dcf19d09aae7a459
aliceco <71007397+aliceco@users.noreply.github.com>
2021-05-13 21:34:26 +0200

    mobile pages for home and film/photo

--- main.js
+++ main.js
@@ -8,3 +8,10 @@ items.forEach((item) => {
   });
 });
 
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
